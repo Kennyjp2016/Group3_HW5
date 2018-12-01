a988852b294fc11e70faf6fe32eba86b0ae4cb12
Kennyjp2016 <43482403+Kennyjp2016@users.noreply.github.com>
2018-11-30 23:08:25 -0500

    Update speakers.cpp
    Almost complete

--- speakers.cpp
+++ speakers.cpp
@@ -5,5 +5,40 @@
 using namespace std;
 
 struct speaker {
-
+	string name;
+	string email;
+	string phone_number;
+	string topic;
+	int fee;
 };
+
+void Speakers(){
+	speaker event1[10];
+	int count = 0;
+	while (count < 9);
+	cout << "Please enter a speakers name" << endl;
+	cin.ignore();
+
+	getline(cin, event1[count].name);
+	cout << "Please enter their topic" << endl;
+
+	getline(cin, event1[count].topic);
+	cout << "Please enter their fee" << endl;
+
+	event1[count].fee = goodIn(0, 2147483647);
+	cout << "Please enter their phone number" << endl;
+
+	getline(cin, event1[count].phone_number);
+	while (event1[count].phone_number.length != 10) {
+		cout << "Please enter a valid 10 digit phone number including the 1" << endl;
+		getline(cin, event1[count].phone_number);
+	}
+
+	cout << "Please enter their email address" << endl;
+	
+
+
+
+
+}
+
